28d05a5f89413033e05da82b1fd3dfa1947d9fc0
deviyan <dem13725@yahoo.com>
2024-03-20 15:52:55 +0300

    Added second LabWork

---
+++ 353505/Ануфриев Д.И./LabWork_2/Task_3/number.cpp
@@ -0,0 +1,18 @@
+#include "number.h"
+
+Number::Number() {}
+
+Number::~Number()
+{
+
+}
+
+Number::Number(double num)
+{
+    number = num;
+}
+
+double Number::evaluate()
+{
+    return number;
+}
