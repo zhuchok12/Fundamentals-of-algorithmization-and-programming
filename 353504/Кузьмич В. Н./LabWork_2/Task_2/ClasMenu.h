292f5d9810e66830da167f964597912ae001d244
Vlad K <vladkuzmich786@gmail.com>
2024-04-04 22:09:35 +0300

    Solved second lab

---
+++ 353504/Кузьмич В. Н./LabWork_2/Task_2/ClasMenu.h
@@ -0,0 +1,15 @@
+#ifndef CLASMENU_H
+#define CLASMENU_H
+
+#include<QString>
+
+class ClasMenu
+{
+public:
+    ClasMenu();
+    ClasMenu(QString, QString, int);
+    QString nas, opis;
+    int price;
+};
+
+#endif // CLASMENU_H
