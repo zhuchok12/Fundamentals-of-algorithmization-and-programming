3a62fe461802414c3d0b0cbe4311bc4c5ae3fc09
Dzmitry Leushukou <dmitrylevshukov@outlook.com>
2024-04-06 11:20:30 +0300

    Fixed repository

--- 353504/Левшуков Д. А./LabWork_1/Task_2/star6.cpp
+++
@@ -1,9 +0,0 @@
-#include "star6.h"
-
-void star6::setP(QMouseEvent *m)
-{
-    MouseNow=m->pos();
-    n=6;
-    //qDebug()<<"Star5 Setp";
-    SetStarP();
-}
--- 353504/Левшуков Д. А./LabWork_1/Task_2/star7.cpp
+++
@@ -1,9 +0,0 @@
-#include "star7.h"
-
-void star7::setP(QMouseEvent *m)
-{
-    MouseNow=m->pos();
-    n=7;
-    //qDebug()<<"Star5 Setp";
-    SetStarP();
-}
