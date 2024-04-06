e7ee424b77b86d201ccc2ec2a1b4e6e53eecd485
Dmitry Snesar <dmitrysnesar9@gmail.com>
2024-04-06 12:09:33 +0300

    solved 3 lab

---
+++ 353501/Снесарь Д.О./LabWork_3/Task_4/mainwindow.h
@@ -0,0 +1,42 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QGraphicsView>
+#include <QGraphicsScene>
+#include <QGraphicsRectItem>
+#include <QGraphicsEllipseItem>
+#include <QString>
+#include <QThread>
+#include "item.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_startB_clicked();
+    void on_numSB_valueChanged(int arg1);
+
+    void on_pushButton_clicked();
+
+private:
+    QVector<Item*> items;
+    QGraphicsScene* scene;
+    Ui::MainWindow *ui;
+    void hanoi (int quantity, QString from, QString to, QString buf_peg);
+    int numA;
+    int numB;
+    int numC;
+    void numDec(QString from);
+
+};
+#endif // MAINWINDOW_H
