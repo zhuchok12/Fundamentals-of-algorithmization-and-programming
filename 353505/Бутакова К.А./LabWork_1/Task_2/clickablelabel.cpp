f2970a85d287f3415fa252a48dca072d22ade082
Kristina <butakovakristina6@gmail.com>
2024-04-07 10:08:50 +0300

    Solved first lab

---
+++ 353505/Бутакова К.А./LabWork_1/Task_2/clickablelabel.cpp
@@ -0,0 +1,16 @@
+
+#include "clickablelabel.h"
+
+ClickableLabel::ClickableLabel(QWidget *parent)
+{
+    setMouseTracking(true);
+}
+
+void ClickableLabel::mousePressEvent(QMouseEvent *event)
+{
+    QLabel::mousePressEvent(event);
+    if (event->button() == Qt::LeftButton) {
+        hide();
+    }
+}
+
