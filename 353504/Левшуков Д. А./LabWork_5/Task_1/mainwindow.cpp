70b91851843b6af3932918ebac3a8700712fd94f
Dzmitry Leushukou <dmitrylevshukov@outlook.com>
2024-04-27 13:49:47 +0300

    Started over

--- 353504/Левшуков Д. А./LabWork_5/Task_1/mainwindow.cpp
+++ 353504/Левшуков Д. А./LabWork_5/Task_1/mainwindow.cpp
@@ -1,433 +1,170 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#define Button(x) ui->b##x
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    ui->All_good->setHidden(true);
-    ui->All_Good_label->setHidden(true);
-    on_comboBox_currentIndexChanged(0);
 
-    //Set base options of button
-    SetDefaultKeyboard();
-    qDebug()<<toUp[0];
-    ui->BackSpaceButton->setHidden(true);
-    ui->PercentOfText->setValue(0);
-    ui->Accurate->setValue(0);
-    on_checkBox_stateChanged(0);
+    setBelarussian();
 }
-
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
 
-
-void MainWindow::gen_text()
+void MainWindow::setBelarussian()
 {
-    task="";
-    time_t t=time(NULL);
-    std::mt19937 gen(t);
-    for(int i=0;i<size;i++)
+    alphabet="Ё            ЙЦУКЕНГШЎЗХ ФЫВАПРОЛДЖЭЯЧСМІТЬБЮ ё            йцукенгшўзх фывапролджэячсмітьбю ";
+
+    Caps_on=false;
+    Shift_on=false;
+
+    clearKeyboard();
+
+
+}
+
+QPushButton *MainWindow::getButton(int ind)
+{
+    if(ind==1)
+        return Button(1);
+    if(ind==2)
+        return Button(2);
+    if(ind==3)
+        return Button(3);
+    if(ind==4)
+        return Button(4);
+    if(ind==5)
+        return Button(5);
+    if(ind==6)
+        return Button(6);
+    if(ind==7)
+        return Button(7);
+    if(ind==8)
+        return Button(8);
+    if(ind==9)
+        return Button(9);
+    if(ind==10)
+        return Button(10);
+    if(ind==11)
+        return Button(11);
+    if(ind==12)
+        return Button(12);
+    if(ind==13)
+        return Button(13);
+    if(ind==14)
+        return Button(14);
+    if(ind==15)
+        return Button(15);
+    if(ind==16)
+        return Button(16);
+    if(ind==17)
+        return Button(17);
+    if(ind==18)
+        return Button(18);
+    if(ind==19)
+        return Button(19);
+    if(ind==20)
+        return Button(20);
+    if(ind==21)
+        return Button(21);
+    if(ind==22)
+        return Button(22);
+    if(ind==23)
+        return Button(23);
+    if(ind==24)
+        return Button(24);
+    if(ind==25)
+        return Button(25);
+    if(ind==26)
+        return Button(26);
+    if(ind==27)
+        return Button(27);
+    if(ind==28)
+        return Button(28);
+    if(ind==29)
+        return Button(29);
+    if(ind==30)
+        return Button(30);
+    if(ind==31)
+        return Button(31);
+    if(ind==32)
+        return Button(32);
+    if(ind==33)
+        return Button(33);
+    if(ind==34)
+        return Button(34);
+    if(ind==35)
+        return Button(35);
+    if(ind==36)
+        return Button(36);
+    if(ind==37)
+        return Button(37);
+    if(ind==38)
+        return Button(38);
+    if(ind==39)
+        return Button(39);
+    if(ind==40)
+        return Button(40);
+    if(ind==41)
+        return Button(41);
+    if(ind==42)
+        return Button(42);
+    if(ind==43)
+        return Button(43);
+    if(ind==44)
+        return Button(44);
+    if(ind==45)
+        return Button(45);
+    if(ind==46)
+        return Button(46);
+    //Caps
+    if(ind==47)
+        return ui->CapsLock;
+    //Shift
+    if(ind==48)
+        return ui->Shift;
+    //Shift
+    if(ind==49)
+        return ui->Space;
+}
+
+void MainWindow::clearKeyboard()
+{
+    for(int i=1;i<=49;i++)
     {
-        QChar g=alphabet[language][gen()%alphabet[language].size()];
-        if(g==' '&&task.size()>0&&task.back()==' ')//double space
-        {
-            i--;
-            continue;
-        }
-        if(g==' '&&i==0)//space in begining
-        {
-            i--;
-            continue;
-        }
-        task+=g;
-
+        getButton(i)->setStyleSheet(default_button);
+        if(i<=46)
+        getButton(i)->setText(alphabet[i-1]);
     }
-
-    while(task.back()==' ')// space in the end
-    {
-        task.back()=alphabet[language][gen()%alphabet[language].size()];
-    }
-    ui->textBrowser->setText(task);
 }
-
-
-bool MainWindow::index_of_button(int &ind,int b)
-{
-    for(int i=0;i<39;i++)
-        if(but[i]==b)
-        {
-            ind=i;
-            return true;
-        }
-    return false;
-}
-
-
-QPushButton* MainWindow::get(int index)
-{
-    //qDebug()<<index<<" "<<but[36];
-    if(index==but[0])
-        return ui->TildaButton;
-    if(index==but[1])
-        return ui->QButton;
-    if(index==but[2])
-        return ui->WButton;
-    if(index==but[3])
-        return ui->EButton;
-    if(index==but[4])
-        return ui->RButton;
-    if(index==but[5])
-        return ui->TButton;
-    if(index==but[6])
-        return ui->YButton;
-    if(index==but[7])
-        return ui->UButton;
-    if(index==but[8])
-        return ui->IButton;
-    if(index==but[9])
-        return ui->OButton;
-    if(index==but[10])
-        return ui->PButton;
-    if(index==but[11])
-        return ui->P1Button;
-    if(index==but[12])
-        return ui->P2Button;
-    if(index==but[13])
-        return ui->AButton;
-    if(index==but[14])
-        return ui->SButton;
-    if(index==but[15])
-        return ui->DButton;
-    if(index==but[16])
-        return ui->FButton;
-    if(index==but[17])
-        return ui->GButton;
-    if(index==but[18])
-        return ui->HButton;
-    if(index==but[19])
-        return ui->JButton;
-    if(index==but[20])
-        return ui->KButton;
-    if(index==but[21])
-        return ui->LButton;
-    if(index==but[22])
-        return ui->L1Button;
-    if(index==but[23])
-        return ui->L2Button;
-    if(index==but[24])
-        return ui->ZButton;
-    if(index==but[25])
-        return ui->XButton;
-    if(index==but[26])
-        return ui->CButton;
-    if(index==but[27])
-        return ui->VButton;
-    if(index==but[28])
-        return ui->BButton;
-    if(index==but[29])
-        return ui->NButton;
-    if(index==but[30])
-        return ui->MButton;
-    if(index==but[31])
-        return ui->M1Button;
-    if(index==but[32])
-        return ui->M2Button;
-    if(index==but[33])
-        return ui->M3Button;
-    if(index==but[34])
-        return ui->MinusButton;
-    if(index==but[35])
-        return ui->CapsButton;
-    if(index==but[36])
-        return ui->ShiftButton;
-    if(index==but[37])
-        return ui->SpaceButton;
-    if(index==but[38])
-        return ui->BackSpaceButton;
-}
-
 
 void MainWindow::on_comboBox_currentIndexChanged(int index)
 {
-    //qDebug()<<"Language changed:"<<index;
-    if(index>1)return;
-    language=index;
-    gen_text();
-    change_keyboard();
-    update();
+    //qDebug()<<index;
+    if(index==0)//Belarussian
+    {
+        setBelarussian();
+    }
 }
 
-
-void MainWindow::change_keyboard()
+void MainWindow::on_checkBox_stateChanged(int arg1)
 {
-    switch(language)
-    {
-    case 0:
-        set_belarusian_keyboard();
-        break;
-    case 1:
-        set_german_keyboard();
-        break;
-
-    case 2:
-    {
-
-        break;
-    }
-    case 3:
-        break;
-
-    case 4:
-        break;
-
-    case 5:
-        break;
-    }
-    if(!Hiden)
-        ShowUnusedKeys();
+    if(arg1==2)
+        for(int i=1;i<=49;i++)
+        {
+            if(getButton(i)->text()==" ")
+                getButton(i)->hide();
+        }
     else
-        HideUnusedKeys();
+        for(int i=1;i<=49;i++)
+        {
+                getButton(i)->show();
+        }
 }
 
-
-void MainWindow::set_belarusian_keyboard()
-{
-    ui->TildaButton->setText("Ё");
-    ui->QButton->setText("Й");
-    ui->WButton->setText("Ц");
-    ui->EButton->setText("У");
-    ui->RButton->setText("К");
-    ui->TButton->setText("Е");
-    ui->YButton->setText("Н");
-    ui->UButton->setText("Г");
-    ui->IButton->setText("Ш");
-    ui->OButton->setText("Ў");
-    ui->PButton->setText("З");
-    ui->P1Button->setText("Х");
-    ui->P2Button->setText("");
-    ui->AButton->setText("Ф");
-    ui->SButton->setText("Ы");
-    ui->DButton->setText("В");
-    ui->FButton->setText("А");
-    ui->GButton->setText("П");
-    ui->HButton->setText("Р");
-    ui->JButton->setText("О");
-    ui->KButton->setText("Л");
-    ui->LButton->setText("Д");
-    ui->L1Button->setText("Ж");
-    ui->L2Button->setText("Э");
-    ui->ZButton->setText("Я");
-    ui->XButton->setText("Ч");
-    ui->CButton->setText("С");
-    ui->VButton->setText("М");
-    ui->BButton->setText("І");
-    ui->NButton->setText("Т");
-    ui->MButton->setText("Ь");
-    ui->M1Button->setText("Б");
-    ui->M2Button->setText("Ю");
-    ui->M3Button->setText("");
-    ui->MinusButton->setText("");
-    //qDebug()<<ui->M3Button->text();
-    update();
-}
-
-void MainWindow::set_german_keyboard()
-{
-    ui->TildaButton->setText("");
-    ui->QButton->setText("Q");
-    ui->WButton->setText("W");
-    ui->EButton->setText("E");
-    ui->RButton->setText("R");
-    ui->TButton->setText("T");
-    ui->YButton->setText("Z");
-    ui->UButton->setText("U");
-    ui->IButton->setText("I");
-    ui->OButton->setText("O");
-    ui->PButton->setText("P");
-    ui->P1Button->setText("Ü");
-    ui->P2Button->setText("");
-    ui->AButton->setText("A");
-    ui->SButton->setText("S");
-    ui->DButton->setText("D");
-    ui->FButton->setText("F");
-    ui->GButton->setText("G");
-    ui->HButton->setText("H");
-    ui->JButton->setText("J");
-    ui->KButton->setText("K");
-    ui->LButton->setText("L");
-    ui->L1Button->setText("Ö");
-    ui->L2Button->setText("Ä");
-    ui->ZButton->setText("");
-    ui->XButton->setText("Y");
-    ui->CButton->setText("X");
-    ui->VButton->setText("C");
-    ui->BButton->setText("V");
-    ui->NButton->setText("B");
-    ui->MButton->setText("N");
-    ui->M1Button->setText("M");
-    ui->M2Button->setText("");
-    ui->M3Button->setText("");
-    ui->MinusButton->setText("ß");
-    //qDebug()<<ui->M3Button->text();
-    update();
-}
-
-void MainWindow::set_french_keyboard()
-{
-
-    ui->TildaButton->setText("");
-    ui->QButton->setText("A");
-    ui->WButton->setText("Z");
-    ui->EButton->setText("E");
-    ui->RButton->setText("R");
-    ui->TButton->setText("T");
-    ui->YButton->setText("Z");
-    ui->UButton->setText("U");
-    ui->IButton->setText("I");
-    ui->OButton->setText("O");
-    ui->PButton->setText("P");
-    ui->P1Button->setText("Ü");
-    ui->P2Button->setText("");
-    ui->AButton->setText("A");
-    ui->SButton->setText("S");
-    ui->DButton->setText("D");
-    ui->FButton->setText("F");
-    ui->GButton->setText("G");
-    ui->HButton->setText("H");
-    ui->JButton->setText("J");
-    ui->KButton->setText("K");
-    ui->LButton->setText("L");
-    ui->L1Button->setText("Ö");
-    ui->L2Button->setText("Ä");
-    ui->ZButton->setText("");
-    ui->XButton->setText("Y");
-    ui->CButton->setText("X");
-    ui->VButton->setText("C");
-    ui->BButton->setText("V");
-    ui->NButton->setText("B");
-    ui->MButton->setText("N");
-    ui->M1Button->setText("M");
-    ui->M2Button->setText("");
-    ui->M3Button->setText("");
-    ui->MinusButton->setText("ß");
-    //qDebug()<<ui->M3Button->text();
-    update();
-}
-
-
-void MainWindow::on_pushButton_clicked()
-{
-    ui->pushButton->setEnabled(false);
-    ui->pushButton->setHidden(true);
-    update();
-    gen_text();
-    ui->pushButton->setHidden(false);
-    ui->pushButton->setEnabled(true);
-    update();
-}
-
-
-void MainWindow::keyPressEvent(QKeyEvent *ke)
-{
-    int key=ke->key();
-    int ind=0;
-    //qDebug()<<"+"<<ke->key();
-    if(!index_of_button(ind,key))return;
-
-    get(key)->setStyleSheet(pressed_button);
-    qDebug()<<"+"<<ke->key();
-}
-
-void MainWindow::PercentOfText()
-{
-
-}
-
-
-void MainWindow::SetTime()
-{
-    ui->Time->setText(QString::fromStdString(std::to_string(clock()-start)));
-}
-
-
-void MainWindow::Start()
-{
-    ui->PercentOfText->setValue(0);
-    ui->Accurate->setValue(0);
-    start=clock();
-    ui->speed->setText("");
-}
-
-void MainWindow::SetDefaultKeyboard()
-{
-    for(int i=0;i<39;i++)
-    {
-        get(but[i])->setStyleSheet(default_button);
-        get(but[i])->setFocusPolicy(Qt::NoFocus);
-    }
-    CapsActive=false;
-}
-
-void MainWindow::ChangeCapsState()
-{
-    if(CapsActive)
-        CapsActive=false;
-    else CapsActive=true,ui->CapsButton->setStyleSheet(CapsOn);
-
-}
-
-
-void MainWindow::keyReleaseEvent(QKeyEvent *ke)
-{
-    int key=ke->key();
-    int ind=0;
-    if(!index_of_button(ind,key))return;
-
-    get(key)->setStyleSheet(default_button);
-    if(ke->key()==16777252)
-        ChangeCapsState();
-    else
-        qDebug()<<"-"<<ke->key()<<" "<<key;
-}
-
-void MainWindow::on_checkBox_stateChanged(int arg1)//0 - no 2- yes
-{
-    ShowUnusedKeys();
-    if(arg1==0)
-    {
-        HideUnusedKeys();
-        Hiden=true;
-    }
-    else
-    {
-
-        Hiden=false;
-    }
-
-    //qDebug()<<arg1;
-}
-
-void MainWindow::HideUnusedKeys()
-{
-    ShowUnusedKeys();
-    for(int i=0;i<36;i++)
-    {
-        //qDebug()<<get(but[i])->text();
-        if(get(but[i])->text()=="")
-            get(but[i])->setHidden(true);
-    }
-}
-
-void MainWindow::ShowUnusedKeys()
-{
-    for(int i=0;i<36;i++)
-    {
-        //if(get(b[i])->text()=="")
-            get(but[i])->setHidden(false);
-    }
-}
--- 353504/Левшуков Д. А./LabWork_5/Task_1/mainwindow.h
+++ 353504/Левшуков Д. А./LabWork_5/Task_1/mainwindow.h
@@ -2,12 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-#include <ctime>
-#include <random>
-#include <QKeyEvent>
 #include <QPushButton>
-//#include <QVirtualKeyboardInputContext>
-
 QT_BEGIN_NAMESPACE
 namespace Ui {
 class MainWindow;
@@ -21,51 +16,29 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
-    void gen_text();
-    bool index_of_button(int& ind,int b);
-    QPushButton* get(int index);
+
+    void setBelarussian();
+    QPushButton* getButton(int ind);
+    void clearKeyboard();
+
 private slots:
     void on_comboBox_currentIndexChanged(int index);
-    void change_keyboard();
-    void on_pushButton_clicked();
-    virtual void keyReleaseEvent(QKeyEvent *ke) override;
-    virtual void keyPressEvent(QKeyEvent *ke) override;
-    void PercentOfText();
-    void SetTime();
-    void Start();
-    void SetDefaultKeyboard();
-    void ChangeCapsState();
-    // /pus
+
     void on_checkBox_stateChanged(int arg1);
-    void HideUnusedKeys();
-    void ShowUnusedKeys();
-    //Language sets
-    void set_belarusian_keyboard();
-    void set_german_keyboard();
-    void set_french_keyboard();
+
 private:
     Ui::MainWindow *ui;
-    const int size=1000;//size of text_browser
-    const QString alphabet[3]={"ёйцукенгшўзхфывапролджэячсмітьбюЁЙЦУКЕНГШЎЗХФЫВАПРОЛДЖЭЯЧСМІТЬБЮ                                     ",
-                               "ABCDEFGHIJKLMNOPQRSTUVWXYZÄÖÜßabcdefghijklmnopqrstuvwxyzäöü                                     ",
-                               "ABCDEFGHIJKLMNOP"
-                              };
-    //0-Belarusian 1-Deutsh
-    const int toUp[2]={32,0};
-    QString task;
-    int language=0;
 
-    time_t start;
     //Stylsheets
     const QString default_button="background-color: rgb(143, 240, 164);color: rgb(0, 0, 0);";
     const QString pressed_button="background-color: rgb(246, 97, 81);color: rgb(0, 0, 0);";
     const QString CapsOn="background-color: rgb(248, 228, 92);color: rgb(0, 0, 0);";
-    //Buttons
-    const int but[39]={96, 81, 87, 69, 82, 84, 89, 85, 73, 79, 80, 91, 93, 65, 83, 68, 70, 71, 72, 74, 75, 76, 59, 39, 90, 88, 67, 86, 66, 78, 77, 44, 46, 47,45, 16777252, 16777248, 32, 16777219};
-    //Last 5:minus caps shift space backspace
-    bool CapsActive=false;
-    bool Hiden=false;
 
+    //Alphabet
+    QString alphabet;
+    const short int delta=46;//Upper+46==lower
+
+    bool Shift_on,Caps_on;
 
 };
 #endif // MAINWINDOW_H
