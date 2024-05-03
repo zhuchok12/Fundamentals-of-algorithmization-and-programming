5fd1846f7aa363385ce4d818d4cf21ece7287717
KNovak <knovaksenija@gmail.com>
2024-05-03 18:21:57 +0300

    Done lab 2

---
+++ 353504/Новак К. А./LabWork_2/Task_1/main-2.cpp
@@ -0,0 +1,11 @@
+#include "mainwindow-2.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
